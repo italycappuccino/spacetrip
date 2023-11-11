f2810ffb8a359975348f6806a168d9dce4cc52cf
Peter <italycappuccino@gmail.com>
2023-11-12 02:25:05 +0800

    init project

--- body/src/test/java/com/spacetrip/BootApplicationTests.java
+++ body/src/test/java/com/spacetrip/BootApplicationTests.java
@@ -1,13 +1,17 @@
 package com.spacetrip;
 
+import com.alibaba.fastjson2.JSON;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class BootApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println(JSON.toJSONString(Arrays.asList("hello", "world")));
 	}
 
 }
